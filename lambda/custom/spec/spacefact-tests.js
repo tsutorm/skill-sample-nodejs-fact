4c88bcaf35428e188e990b8e6dbed857d1850f33
onishi.tsutomu <onishi.tsutomu@a-tm.co.jp>
2017-12-21 03:24:50 +0900

    add alexa-skill-test-framework

---
+++ lambda/custom/spec/spacefact-tests.js
@@ -0,0 +1,121 @@
+
+/*
+Mocha tests for the Alexa skill "Space Facts" example (https://github.com/alexa/skill-sample-nodejs-fact).
+Using the Alexa Skill Test Framework (https://github.com/BrianMacIntosh/alexa-skill-test-framework).
+
+Run with 'mocha examples/skill-sample-nodejs-fact/spacefact-tests.js'.
+*/
+
+// include the testing framework
+const alexaTest = require('alexa-skill-test-framework');
+//const alexaTest = require('../../index');
+
+// initialize the testing framework
+alexaTest.initialize(
+	require('../index.js'),
+	"amzn1.ask.skill.00000000-0000-0000-0000-000000000000",
+	"amzn1.ask.account.VOID");
+
+// initialize i18n
+var textResources = require("../resources.js");
+alexaTest.initializeI18N(textResources);
+
+var supportedLocales = ["en-US", "en-GB", "de-DE", "ja-JP"];
+
+// perform each test in each supported language
+for (var i = 0; i < supportedLocales.length; i++) {
+	var locale = supportedLocales[i];
+
+	// set the language
+	alexaTest.setLocale(locale);
+
+	// callback function that asserts if the provided string is not a fact from the list
+	var assertIfNotFact = function (context, suspectedFact) {
+		var facts = context.t("FACTS");
+		for (var i = 0; i < facts.length; i++) {
+			if (suspectedFact === "<speak> " + context.t("GET_FACT_MESSAGE") + facts[i] + " </speak>") return;
+		}
+		context.assert({ message: "'" + suspectedFact + "' is not a space fact." });
+	}
+
+	describe("Space Fact Skill (" + locale + ")", function () {
+		// tests the behavior of the skill's LaunchRequest
+		describe("LaunchRequest", function () {
+			alexaTest.test([
+				{
+					request: alexaTest.getLaunchRequest(), shouldEndSession: true, repromptsNothing: true,
+					saysCallback: assertIfNotFact
+				}
+			]);
+		});
+
+		// tests the behavior of the skill's GetNewFactIntent
+		describe("GetNewFactIntent", function () {
+			alexaTest.test([
+				{
+					request: alexaTest.getIntentRequest("GetNewFactIntent"),
+					saysCallback: assertIfNotFact, shouldEndSession: true, repromptsNothing: true,
+					hasCardTitle: alexaTest.t("SKILL_NAME")
+				}
+			]);
+		});
+
+		// tests the behavior of the skill's AMAZON.HelpIntent
+		describe("AMAZON.HelpIntent into GetNewFactIntent", function () {
+			alexaTest.test([
+				{
+					request: alexaTest.getIntentRequest("AMAZON.HelpIntent"),
+					says: alexaTest.t("HELP_MESSAGE"), shouldEndSession: false, reprompts: alexaTest.t("HELP_REPROMPT")
+				},
+				{
+					request: alexaTest.getIntentRequest("GetNewFactIntent"),
+					saysCallback: assertIfNotFact, shouldEndSession: true, repromptsNothing: true
+				}
+			]);
+		});
+		describe("AMAZON.HelpIntent into AMAZON.CancelIntent", function () {
+			alexaTest.test([
+				{
+					request: alexaTest.getIntentRequest("AMAZON.HelpIntent"),
+					says: alexaTest.t("HELP_MESSAGE"), shouldEndSession: false, reprompts: alexaTest.t("HELP_REPROMPT")
+				},
+				{
+					request: alexaTest.getIntentRequest("AMAZON.CancelIntent"),
+					says: alexaTest.t("STOP_MESSAGE"), shouldEndSession: true, repromptsNothing: true
+				}
+			]);
+		});
+		describe("AMAZON.HelpIntent into AMAZON.StopIntent", function () {
+			alexaTest.test([
+				{
+					request: alexaTest.getIntentRequest("AMAZON.HelpIntent"),
+					says: alexaTest.t("HELP_MESSAGE"), shouldEndSession: false, reprompts: alexaTest.t("HELP_REPROMPT"),
+				},
+				{
+					request: alexaTest.getIntentRequest("AMAZON.StopIntent"),
+					says: alexaTest.t("STOP_MESSAGE"), shouldEndSession: true, repromptsNothing: true
+				}
+			]);
+		});
+
+		// tests the behavior of the skill's AMAZON.CancelIntent
+		describe("AMAZON.CancelIntent", function () {
+			alexaTest.test([
+				{
+					request: alexaTest.getIntentRequest("AMAZON.CancelIntent"),
+					says: alexaTest.t("STOP_MESSAGE"), shouldEndSession: true, repromptsNothing: true
+				}
+			]);
+		});
+
+		// tests the behavior of the skill's AMAZON.StopIntent
+		describe("AMAZON.StopIntent", function () {
+			alexaTest.test([
+				{
+					request: alexaTest.getIntentRequest("AMAZON.StopIntent"),
+					says: alexaTest.t("STOP_MESSAGE"), shouldEndSession: true, repromptsNothing: true
+				}
+			]);
+		});
+	});
+}
