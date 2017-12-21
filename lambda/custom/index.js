6ff4263dfcfbe44397e0d199b101e8f205e62723
onishi.tsutomu <onishi.tsutomu@a-tm.co.jp>
2017-12-21 09:39:19 +0900

    modify

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -7,7 +7,7 @@ var Alexa = require('alexa-sdk');
 
 //Replace with your app ID (OPTIONAL).  You can find this value at the top of your skill's page on http://developer.amazon.com.  
 //Make sure to enclose your value in quotes, like this: var APP_ID = "amzn1.ask.skill.bb4045e6-b3e8-4133-b650-72923c5980f1";
-var APP_ID = undefined;
+var APP_ID = process.env.APP_ID;
 
 //=========================================================================================================================================
 //この行から下のコードに変更を加えると、スキルが動作しなくなるかもしれません。わかる人のみ変更を加えてください。  
